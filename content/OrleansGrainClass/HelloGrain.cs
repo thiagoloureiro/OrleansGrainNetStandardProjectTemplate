fbf92e0ebc7575110cf4cd84bbdfcc2ab3767a9a
Thiago Loureiro <thiagoguaru@me.com>
2018-10-28 22:01:09 +0100

    Initial Commit

---
+++ content/OrleansGrainClass/HelloGrain.cs
@@ -0,0 +1,14 @@
+﻿using Orleans;
+using System.Threading.Tasks;
+
+namespace OrleansGrainClass
+{
+    // Add reference to the Interface and Implement the interface in this class
+    public class HelloGrain : Grain //,IHello
+    {
+        private Task<string> SayHello(string greeting)
+        {
+            return Task.FromResult($"{greeting}, welcome to Orleans!");
+        }
+    }
+}
